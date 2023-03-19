b9e4bacd24a2f7d4e6497a8e6221947d6ec01e56
Alan Ben <abseralvo@gmail.com>
2023-03-19 15:06:07 +0200

    with .dep

--- scripts/dev/typescript-oo/classes/FrontendPackage.class.sh
+++ scripts/dev/typescript-oo/classes/FrontendPackage.class.sh
@@ -39,22 +39,23 @@ FrontendPackage() {
 
   _compile() {
     logInfo "Compiling: ${folderName}"
+        for lib in ${@}; do
+          [[ "${lib}" == "${_this}" ]] && break
+          local libPath="$("${lib}.path")"
+          local libFolderName="$("${lib}.folderName")"
+          local libPackageName="$("${lib}.packageName")"
+
+          [[ ! "$(cat package.json | grep "${libPackageName}")" ]] && continue
+
+          local backendDependencyPath="./.dependencies/${libFolderName}"
+          createDir "${backendDependencyPath}"
+          cp -rf "${libPath}/${libFolderName}/${outputDir}"/* "${backendDependencyPath}/"
+        done
 
     npm run build
     throwWarning "Error compiling: ${folderName}"
 
-#    for lib in ${@}; do
-#      [[ "${lib}" == "${_this}" ]] && break
-#      local libPath="$("${lib}.path")"
-#      local libFolderName="$("${lib}.folderName")"
-#      local libPackageName="$("${lib}.packageName")"
-#
-#      [[ ! "$(cat package.json | grep "${libPackageName}")" ]] && continue
-#
-#      local backendDependencyPath="./.dependencies/${libFolderName}"
-#      createDir "${backendDependencyPath}"
-#      cp -rf "${libPath}/${libFolderName}/${outputDir}"/* "${backendDependencyPath}/"
-#    done
+
   }
 
   _lint() {
