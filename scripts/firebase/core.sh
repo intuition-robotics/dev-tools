1dd4449ab829e6cf12a4f4462735ee89f44dc9fa
AndreiHardziyenkaIR <andrei.hardziyenka@intuitionrobotics.com>
2024-10-16 16:28:01 +0300

    saving

--- scripts/firebase/core.sh
+++ scripts/firebase/core.sh
@@ -9,6 +9,7 @@ verifyFirebaseProjectIsAccessible() {
 
   logDebug "Verifying You are logged in to firebase tools...'"
   if [[ "${USER,,}" != "jenkins" ]] && [[ "${USER,,}" != "runner" ]]; then
+    ### NOT NEEDED IN K8S
      $(resolveCommand firebase) login:ci
   fi
 
